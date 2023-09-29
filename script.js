600fbd2312968678e57fa0d42a875b51b9e1f99d
Fernando Araújo Pires Magalhães <d2021030640@unifei.edu.br>
2023-09-29 15:19:59 -0300

    Launch

---
+++ script.js
@@ -0,0 +1,8 @@
+const point = document.getElementById("point");
+const platform = document.querySelector(".platform");
+
+function playAudio() {
+    var audio = new Audio('assets/audio/Sonic.mp3'); // Substitua 'caminho-para-seu-arquivo-de-audio.mp3' pelo caminho do seu arquivo de áudio
+    audio.play();
+}
+playAudio();
